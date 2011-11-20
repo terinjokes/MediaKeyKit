a2554b175ded27dd1f9d25b8d6c1a4879e67c623
Terin Stock <terinjokes@gmail.com>
2011-11-19 21:35:33 -0500

    First Commit

---
+++ MediaKeyKit/MediaKeyKit.h
@@ -0,0 +1,13 @@
+//
+//  MediaKeyKit.h
+//  MediaKeyKit
+//
+//  Created by Terin Stock on 11/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MediaKeyKit : NSObject
+
+@end
---
+++ MediaKeyKit/MediaKeyKit.m
@@ -0,0 +1,23 @@
+//
+//  MediaKeyKit.m
+//  MediaKeyKit
+//
+//  Created by Terin Stock on 11/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MediaKeyKit.h"
+
+@implementation MediaKeyKit
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
